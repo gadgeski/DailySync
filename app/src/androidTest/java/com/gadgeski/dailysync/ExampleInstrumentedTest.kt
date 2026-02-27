855328c5661b1a0ed153e5464dd7efded3fa4b14
gadgeski <xtwh.ce5bil@gmail.com>
2026-02-27 23:34:40 +0900

    fix: correct package name in ExampleInstrumentedTest

--- app/src/androidTest/java/com/gadgeski/dailysync/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/gadgeski/dailysync/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.dailysync", appContext.packageName)
+        assertEquals("com.gadgeski.dailysync", appContext.packageName)
     }
 }
